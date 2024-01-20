27c0fb2a22a359eefe7862a0cb593e0d158998d0
AliSami6 <82383576+AliSami6@users.noreply.github.com>
2024-01-20 12:20:03 +0600

    follow tailwind docs

---
+++ tutorial_01/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   darkMode: 'class',
+  content: ['*'],
+  theme: {
+    screens:{
+      sm:'640px',
+    },
+    // colors:{
+    //   'green':'#686de0',
+    // },
+    fontFamily:{
+      serif: ['Playfair Display','serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
---
+++ tutorial_02/moon.js
@@ -0,0 +1,6 @@
+const darkMode = document.getElementById('dark');
+let mainElement = document.getElementById('main');
+darkMode.addEventListener("click",()=>{
+   
+    mainElement.classList.toggle('dark');
+})
